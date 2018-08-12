21c373346ab841049442608ebc1be36b5b01ffd3
Jeremy Hicks <reysic@me.com>
2018-08-12 13:07:52 -0500

    Formatting

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 // Import config.js (holds token and command prefix)
-// const config = require('./config.json');
+const config = require('./config.json');
 
 // Import the fs native module
 const fs = require('fs');
@@ -34,11 +34,11 @@ client.on('guildMemberAdd', member => {
 
 client.on('message', message => {
   // Return early is command prefix is not present or the message author is another bot (prevent botception)
-  // if (!message.content.startsWith(config.prefix) || message.author.bot) return;
-  if (!message.content.startsWith(process.env.prefix) || message.author.bot) return;
+  if (!message.content.startsWith(config.prefix) || message.author.bot) return;
+  // if (!message.content.startsWith(process.env.prefix) || message.author.bot) return;
 
-  // const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
-  const args = message.content.slice(process.env.prefix.length).trim().split(/ +/g);
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  // const args = message.content.slice(process.env.prefix.length).trim().split(/ +/g);
   // shift() removes one element from the array and returns it
   const command = args.shift().toLowerCase();
 
@@ -70,11 +70,11 @@ client.on('message', message => {
 
   // !user
   if (command === "user") {
-    let member = message.mentions.members.first();
-    message.channel.send(".displayName: ${member.displayName}, .id: ${member.id}, .nickname: {$member.nickname}, .joinedAt: {$member.joinedAt}");
+    let member = message.mentions.member.first();
+    message.channel.send('.displayName: ${member.displayName}, .id: ${member.id}, .nickname: ${member.nickname}, .joinedAt: ${member.joinedAt}');
   }
 });
 
 // Log our bot in using the token from https://discordapp.com/developers/applications/me
-// client.login(config.token);
-client.login(process.env.token);
+client.login(config.token);
+// client.login(process.env.token);
