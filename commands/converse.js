2fbbb95131978b0069c9fc51077ad51362194228
Jeremy Hicks <jeremy@reysic.com>
2018-09-04 15:57:05 -0500

    Debugging converse command

--- commands/converse.js
+++ commands/converse.js
@@ -1,18 +1,21 @@
 // !converse
 const snekfetch = require('snekfetch');
 
-exports.run = (client, message, args) => {
+exports.run = converse;
+
+async function converse(client, message, args) {
   if (!args.length) {
-    message.channel.send("You must provide an argument with this command.").catch(console.error);
+    message.channel.send("You must provide an argument with this command.");
+    return;
   }
 
   var header = {
-    "Authorization": process.env.authorization,
+    "Authorization": "Token " + process.env.authorization,
     "Content-Type": "application/json"
   }
 
-  var dt = {"message": {"content": args, "type": "text"}, "conversation_id": "CONVERSATION_ID"}
+  var dt = '{"message": {"content": "' + args.join(" ") + '", "type": "text"}, "conversation_id": "CONVERSATION_ID"}'
 
   const { body } = await snekfetch.post("https://api.recast.ai/build/v1/dialog", { headers: header, data: dt });
-  message.channel.send(body);
+  message.channel.send(body.results.messages[0].content);
 }
